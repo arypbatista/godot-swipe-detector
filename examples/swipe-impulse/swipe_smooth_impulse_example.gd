cb00c71190b0a7fe281d96e56e4528cd508872dc
Ary Pablo Batista <arypbatista@gmail.com>
2017-02-06 14:45:10 -0300

    Fixed some issues related to refering an object as a boolean condition

--- examples/swipe-impulse/swipe_smooth_impulse_example.gd
+++ examples/swipe-impulse/swipe_smooth_impulse_example.gd
@@ -12,13 +12,13 @@ func get_object(point):
 func _on_SwipeDetector_swipe_started( partial_gesture ):
 	var point = partial_gesture.last_point()
 	sliding_object = get_object(point)
-	if sliding_object:
+	if sliding_object != null:
 		print('We have a sliding object on: ', sliding_object.get_pos())
 
 
 func _on_SwipeDetector_swipe_updated_with_delta( partial_gesture, delta ):
 	var point = partial_gesture.last_point()
-	if sliding_object:
+	if sliding_object != null:
 		var distance = sliding_object.get_pos().distance_to(point)
 		var angle = sliding_object.get_pos().angle_to_point(point)
 		var speed = distance / (delta * 100)
